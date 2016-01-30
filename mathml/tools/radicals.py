8c65567c5ce2dd57a0b389a78080eb66c973304d
Frédéric Wang <fred.wang@free.fr>
2016-01-30 11:47:30 +0100

    Add size variant and glyph assembly for all radical fonts.

--- mathml/tools/radicals.py
+++ mathml/tools/radicals.py
@@ -3,12 +3,25 @@
 import mathfont
 import fontforge
 
-RadicalCodePoint = 0x221a
+def createStretchyRadical(aFont):
+    radicalCodePoint = 0x221a
+    mathfont.createSquareGlyph(aFont, radicalCodePoint)
+    g = aFont.createChar(-1, "size1")
+    mathfont.drawRectangleGlyph(g, mathfont.em, 2 * mathfont.em, 0)
+    g = aFont.createChar(-1, "size2")
+    mathfont.drawRectangleGlyph(g, mathfont.em, 3 * mathfont.em, 0)
+    g = aFont.createChar(-1, "size3")
+    mathfont.drawRectangleGlyph(g, mathfont.em, 4 * mathfont.em, 0)
+    overlap = mathfont.em / 2
+    aFont[radicalCodePoint].verticalVariants = "radical size1 size2 size3"
+    aFont[radicalCodePoint].verticalComponents = \
+        (("size2", False, 0, mathfont.em, 3 * mathfont.em), \
+         ("size1", True, mathfont.em, mathfont.em, 2 * mathfont.em))
 
 v1 = 25
 v2 = 1 * mathfont.em
 f = mathfont.create("radical-degreebottomraisepercent%d-rulethickness%d" % (v1, v2))
-mathfont.createSquareGlyph(f, RadicalCodePoint)
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = v1
 f.math.RadicalDisplayStyleVerticalGap = 0
 f.math.RadicalExtraAscender = 0
@@ -21,7 +34,7 @@ mathfont.save(f)
 v1 = 7 * mathfont.em
 v2 = 1 * mathfont.em
 f = mathfont.create("radical-displaystyleverticalgap%d-rulethickness%d" % (v1, v2))
-mathfont.createSquareGlyph(f, RadicalCodePoint)
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = 0
 f.math.RadicalDisplayStyleVerticalGap = v1
 f.math.RadicalExtraAscender = 0
@@ -34,7 +47,7 @@ mathfont.save(f)
 v1 = 3 * mathfont.em
 v2 = 1 * mathfont.em
 f = mathfont.create("radical-extraascender%d-rulethickness%d" % (v1, v2))
-mathfont.createSquareGlyph(f, RadicalCodePoint)
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = 0
 f.math.RadicalDisplayStyleVerticalGap = 0
 f.math.RadicalExtraAscender = v1
@@ -47,14 +60,7 @@ mathfont.save(f)
 v1 = 5 * mathfont.em
 v2 = 1 * mathfont.em
 f = mathfont.create("radical-kernafterdegreeminus%d-rulethickness%d" % (v1, v2))
-mathfont.createSquareGlyph(f, RadicalCodePoint)
-g = f.createChar(-1, "size1")
-mathfont.drawRectangleGlyph(g, mathfont.em, 2 * mathfont.em, 0)
-g = f.createChar(-1, "size2")
-mathfont.drawRectangleGlyph(g, mathfont.em, 3 * mathfont.em, 0)
-g = f.createChar(-1, "size3")
-mathfont.drawRectangleGlyph(g, mathfont.em, 4 * mathfont.em, 0)
-f[RadicalCodePoint].verticalVariants = "radical size1 size2 size3"
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = 0
 f.math.RadicalDisplayStyleVerticalGap = 0
 f.math.RadicalExtraAscender = 0
@@ -67,7 +73,7 @@ mathfont.save(f)
 v1 = 4 * mathfont.em
 v2 = 1 * mathfont.em
 f = mathfont.create("radical-kernbeforedegree%d-rulethickness%d" % (v1, v2))
-mathfont.createSquareGlyph(f, RadicalCodePoint)
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = 0
 f.math.RadicalDisplayStyleVerticalGap = 0
 f.math.RadicalExtraAscender = 0
@@ -79,7 +85,7 @@ mathfont.save(f)
 
 v = 8 * mathfont.em
 f = mathfont.create("radical-rulethickness%d" % v)
-mathfont.createSquareGlyph(f, RadicalCodePoint)
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = 0
 f.math.RadicalDisplayStyleVerticalGap = 0
 f.math.RadicalExtraAscender = 0
@@ -92,7 +98,7 @@ mathfont.save(f)
 v1 = 6 * mathfont.em
 v2 = 1 * mathfont.em
 f = mathfont.create("radical-verticalgap%d-rulethickness%d" % (v1, v2))
-mathfont.createSquareGlyph(f, RadicalCodePoint)
+createStretchyRadical(f)
 f.math.RadicalDegreeBottomRaisePercent = 0
 f.math.RadicalDisplayStyleVerticalGap = 0
 f.math.RadicalExtraAscender = 0
