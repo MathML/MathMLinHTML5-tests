9c7ba237f72539ffc3e636a3a3d3ef35e8b1f6e0
Frédéric Wang <fred.wang@free.fr>
2016-03-22 18:36:37 +0100

    Add test for AccentBaseHeight and Over/Under bar constants

--- mathml/tools/underover.py
+++ mathml/tools/underover.py
@@ -3,16 +3,20 @@
 from utils import mathfont
 import fontforge
 
-arrowCodePoint = 0x2192 # horizontal stretch operator
+breveCodePoint = 0x2D8 # accent operator
+degreeCodePoint = 0xB0 # nonaccent operator
+accentBaseHeight = 4 * mathfont.em
 
 v = 3 * mathfont.em
-f = mathfont.create("stretchstack-bottomshiftdown%d" % v)
-mathfont.createSquareGlyph(f, arrowCodePoint)
+f = mathfont.create("underover-accentbaseheight%d-overbarextraascender%d" % (accentBaseHeight, v))
+mathfont.createSquareGlyph(f, breveCodePoint)
+mathfont.createSquareGlyph(f, degreeCodePoint)
+f.math.AccentBaseHeight = accentBaseHeight
 f.math.LowerLimitBaselineDropMin = 0
 f.math.LowerLimitGapMin = 0
-f.math.OverbarExtraAscender = 0
+f.math.OverbarExtraAscender = v
 f.math.OverbarVerticalGap = 0
-f.math.StretchStackBottomShiftDown = v
+f.math.StretchStackBottomShiftDown = 0
 f.math.StretchStackGapAboveMin = 0
 f.math.StretchStackGapBelowMin = 0
 f.math.StretchStackTopShiftUp = 0
@@ -23,15 +27,17 @@ f.math.UpperLimitGapMin = 0
 mathfont.save(f)
 
 v = 11 * mathfont.em
-f = mathfont.create("stretchstack-gapbelowmin%d" % v)
-mathfont.createSquareGlyph(f, arrowCodePoint)
+f = mathfont.create("underover-accentbaseheight%d-overbarverticalgap%d" % (accentBaseHeight, v))
+mathfont.createSquareGlyph(f, breveCodePoint)
+mathfont.createSquareGlyph(f, degreeCodePoint)
+f.math.AccentBaseHeight = accentBaseHeight
 f.math.LowerLimitBaselineDropMin = 0
 f.math.LowerLimitGapMin = 0
 f.math.OverbarExtraAscender = 0
-f.math.OverbarVerticalGap = 0
+f.math.OverbarVerticalGap = v
 f.math.StretchStackBottomShiftDown = 0
 f.math.StretchStackGapAboveMin = 0
-f.math.StretchStackGapBelowMin = v
+f.math.StretchStackGapBelowMin = 0
 f.math.StretchStackTopShiftUp = 0
 f.math.UnderbarExtraDescender = 0
 f.math.UnderbarVerticalGap = 0
@@ -40,8 +46,10 @@ f.math.UpperLimitGapMin = 0
 mathfont.save(f)
 
 v = 5 * mathfont.em
-f = mathfont.create("stretchstack-topshiftup%d" % v)
-mathfont.createSquareGlyph(f, arrowCodePoint)
+f = mathfont.create("underover-accentbaseheight%d-underbarextradescender%d" % (accentBaseHeight, v))
+mathfont.createSquareGlyph(f, breveCodePoint)
+mathfont.createSquareGlyph(f, degreeCodePoint)
+f.math.AccentBaseHeight = accentBaseHeight
 f.math.LowerLimitBaselineDropMin = 0
 f.math.LowerLimitGapMin = 0
 f.math.OverbarExtraAscender = 0
@@ -49,26 +57,28 @@ f.math.OverbarVerticalGap = 0
 f.math.StretchStackBottomShiftDown = 0
 f.math.StretchStackGapAboveMin = 0
 f.math.StretchStackGapBelowMin = 0
-f.math.StretchStackTopShiftUp = v
-f.math.UnderbarExtraDescender = 0
+f.math.StretchStackTopShiftUp = 0
+f.math.UnderbarExtraDescender = v
 f.math.UnderbarVerticalGap = 0
 f.math.UpperLimitBaselineRiseMin = 0
 f.math.UpperLimitGapMin = 0
 mathfont.save(f)
 
 v = 7 * mathfont.em
-f = mathfont.create("stretchstack-gapabovemin%d" % v)
-mathfont.createSquareGlyph(f, arrowCodePoint)
+f = mathfont.create("underover-accentbaseheight%d-underbarverticalgap%d" % (accentBaseHeight, v))
+mathfont.createSquareGlyph(f, breveCodePoint)
+mathfont.createSquareGlyph(f, degreeCodePoint)
+f.math.AccentBaseHeight = accentBaseHeight
 f.math.LowerLimitBaselineDropMin = 0
 f.math.LowerLimitGapMin = 0
 f.math.OverbarExtraAscender = 0
 f.math.OverbarVerticalGap = 0
 f.math.StretchStackBottomShiftDown = 0
-f.math.StretchStackGapAboveMin = v
+f.math.StretchStackGapAboveMin = 0
 f.math.StretchStackGapBelowMin = 0
 f.math.StretchStackTopShiftUp = 0
 f.math.UnderbarExtraDescender = 0
-f.math.UnderbarVerticalGap = 0
+f.math.UnderbarVerticalGap = v
 f.math.UpperLimitBaselineRiseMin = 0
 f.math.UpperLimitGapMin = 0
 mathfont.save(f)
