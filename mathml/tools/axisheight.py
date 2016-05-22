f11b6e6bef250ae996f5550c332aa957b6502c71
Frédéric Wang <fred.wang@free.fr>
2016-05-22 15:21:01 +0200

    mo axis height test: also test the case where a glyph assembly is used.

--- mathml/tools/axisheight.py
+++ mathml/tools/axisheight.py
@@ -13,5 +13,11 @@ g = f.createChar(-1, "size1")
 mathfont.drawRectangleGlyph(g, mathfont.em, v2 / 2, 0)
 g = f.createChar(-1, "size2")
 mathfont.drawRectangleGlyph(g, mathfont.em, v2, 0)
+g = f.createChar(-1, "bot")
+mathfont.drawRectangleGlyph(g, mathfont.em, v2 + v1, 0)
+g = f.createChar(-1, "ext")
+mathfont.drawRectangleGlyph(g, mathfont.em, mathfont.em, 0)
 f[verticalArrowCodePoint].verticalVariants = "uni21A8 size1 size2"
+f[verticalArrowCodePoint].verticalComponents = \
+  (("bot", False, 0, 0, mathfont.em), ("ext", True, 0, 0, mathfont.em));
 mathfont.save(f)
